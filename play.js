8f4fdf51f3151909bea58306c1abd89fb0567c94
luanateixeira148 <luanateixeira148@gmail.com>
2021-06-30 22:34:00 +0000

    stdin setup

--- play.js
+++ play.js
@@ -1,4 +1,27 @@
 const { connect } = require("./client.js");
 
 console.log("Connecting ...");
+
 connect();
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  
+  const handleUserInput = function() {
+    // \u0003 maps to ctrl+c input
+    if ('\u0003') {
+      process.exit();
+    }
+  }
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
