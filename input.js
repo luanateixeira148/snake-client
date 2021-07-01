415f24ee3c2cfd253c592a21db9cc40b0916c1a9
luanateixeira148 <luanateixeira148@gmail.com>
2021-07-01 00:16:25 +0000

    added input file - refactoring

---
+++ input.js
@@ -0,0 +1,23 @@
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  
+  const handleUserInput = function() {
+    // \u0003 maps to ctrl+c input
+    if ('\u0003') {
+      process.exit();
+    }
+  }
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+module.exports = {
+  setupInput
+};
--- play.js
+++ play.js
@@ -1,27 +1,7 @@
-const { connect } = require("./client.js");
+const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
-
 connect();
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  
-  const handleUserInput = function() {
-    // \u0003 maps to ctrl+c input
-    if ('\u0003') {
-      process.exit();
-    }
-  }
-
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
 
 setupInput();
