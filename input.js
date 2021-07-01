504161a877080e2ae541982f53c9dd7e5b9731a2
luanateixeira148 <luanateixeira148@gmail.com>
2021-07-01 01:40:55 +0000

    implemented constants

--- input.js
+++ input.js
@@ -1,10 +1,25 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function () {
+const setupInput = function (conn) {
+
+  connection = conn;
   
-  const handleUserInput = function() {
+  const handleUserInput = function(key) {
     // \u0003 maps to ctrl+c input
-    if ('\u0003') {
+    if (key === '\u0003') {
       process.exit();
+    } else if (key === 'w') {
+      connection.write('Move: up')
+    } else if (key === 'a') {
+      connection.write('Move: left')
+    } else if (key === 's') {
+      connection.write('Move: down')
+    } else if (key === 'd') {
+      connection.write('Move: right')
+    } else {
+      connection.write(`Say: ${key}`)
     }
   }
 
